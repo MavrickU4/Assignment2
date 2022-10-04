27da15787d5ccd055e542731d1342f676ac09256
MavrickU4 <tim.upton4@gmail.com>
2022-10-03 21:20:27 -0400

    Assignment2 - setup MongosDB online

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 export const Secret = "MySecret";
+//export const MongoURI  = "mongodb://127.0.0.1/list"
+export const MongoURI  = "mongodb+srv://PersonalProfileDB:<passwordPPDB>@firstcluster.lmcwlwp.mongodb.net/?retryWrites=true&w=majority"
